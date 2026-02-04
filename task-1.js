ad5208fcbac275a086dff3b5e335f4b13b612a37
PhantomOblivion <smasif404@gmail.com>
2026-02-04 20:09:19 +0600

    Solved the string related task-1.

--- task-1.js
+++ task-1.js
@@ -1,2 +1,37 @@
 // Task-1: Count how many times a string has the letter a
 
+
+//>>>Using for loop<<<\\
+
+/*
+let sentence = "I am learning JavaScript.";
+
+// console.log(sentence.length);
+
+let count = 0;
+for(let i = 0; i < sentence.length; i++){
+    if (sentence[i] === 'a') {
+    count ++;
+    }
+}
+console.log(count);
+*/
+
+//>>>Using While Loop<<<\\
+
+/*
+let sentence = "I am a student and I  am learning JavaScript.";
+
+let i = 0;
+let count = 0;
+
+// console.log (sentence.length);
+
+while (i < sentence.length){
+    if(sentence[i] === 'a'){
+        count ++;
+    }
+    i++;
+}
+console.log(count);
+*/
